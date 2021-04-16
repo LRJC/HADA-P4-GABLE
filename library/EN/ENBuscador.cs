3f4bbb444cef6535f9b32a09cfcfd085fd1968ac
nicoslaptopW <nvog1@gcloud.ua.es>
2021-04-16 16:00:11 +0200

    74383864R eliminado Class1.cs y Class2.cs, agregado los ficheros de mis ENs y mis CADs

--- library/EN/ENBuscador.cs
+++ library/EN/ENBuscador.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace library.CAD
+namespace library.EN
 {
-    class Class1
+    class ENBuscador
     {
-        dgfjkasfladf;
     }
 }
