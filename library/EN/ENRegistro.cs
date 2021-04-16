79c36d8e1f58d3b78be6b129859dbea16462601e
Andrés Maciá Gómez <andremacia45@gmail.com>
2021-04-16 15:57:57 +0200

    added Registro "prueba"

---
+++ library/EN/ENRegistro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library.EN;
+{
+    class ENRegistro
+    {
+        private ENUsuario usuario;
+        public ENUsuario usu_
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public ENRegistro()
+        {
+            usu_ = null;
+        }
+
+        public ENRegistro(ENUsuario new_us)
+        {
+            usu_ = new_us;
+        }
+
+        public bool createUsuario()
+        {
+            CADRegistro c = new CADRegistro();
+            return c.createUsuario(*this);
+        }
+    }
+}
