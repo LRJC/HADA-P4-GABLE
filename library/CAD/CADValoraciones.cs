585b748070b0414fe0231f9c753a445cd836cc72
psp59-ua <psp59@gcloud.ua.es>
2021-04-16 11:37:36 +0200

    Corrección de ENProductos y añadido CADProductos,ENValoraciones y CADValoraciones

---
+++ library/CAD/CADValoraciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using library.EN;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library.CAD
+{
+    class CADValoraciones
+    {
+        public CADValoraciones() { }
+
+        public bool createValoraciones(ENValoraciones en) { }
+
+        public bool readValoracioneses(ENValoraciones en) { }
+
+    }
+}
---
+++ library/EN/ENValoraciones.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using library.CAD;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library.EN
+{
+    class ENValoraciones
+    {
+        public string id_val
+        {
+            get
+            {
+                return new string(idVal.ToCharArray());
+            }
+
+            set
+            {
+                idVal = value;
+            }
+        }
+
+        public string tex_val
+        {
+            get
+            {
+                return new string(texto.ToCharArray());
+            }
+
+            set
+            {
+                texto = value;
+            }
+        }
+
+        public int pun_val
+        {
+            get
+            {
+                return puntos;
+            }
+
+            set
+            {
+                puntos = value;
+            }
+        }
+
+
+        private string idVal;
+        private string texto;
+        private int puntos;
+
+        public ENValoraciones() { }
+
+        public ENValoraciones(string idVal, string texto, int puntos)
+        {
+            id_val = idVal;
+            tex_val = texto;
+            pun_val = puntos;
+        }
+        public bool createValoracion()
+        {
+            CADValoraciones cval = new CADValoraciones();
+            return cval.createValoraciones(this);
+        }
+
+        public bool readValoraciones()
+        {
+            CADValoraciones cval = new CADValoraciones();
+            return cval.readValoracioneses(this);
+        }
+    }
+}
