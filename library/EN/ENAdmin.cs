34948747b6ebe155aad15bc6c1a34f5a21f7db51
nicoslaptopW <nvog1@gcloud.ua.es>
2021-04-28 00:29:03 +0200

    74383864R implemented ENAdmin basic structure.

--- library/EN/ENAdmin.cs
+++ library/EN/ENAdmin.cs
@@ -8,5 +8,59 @@ namespace library
 {
     public class ENAdmin
     {
+        private string _dni;
+        public string dni
+        {
+            get { return _dni; }
+            set { _dni = value; }
+        }
+
+        private string _email;
+        public string email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _contrasenya;
+        public string contrasenya
+        {
+            get { return _contrasenya; }
+            set { _contrasenya = value; }
+        }
+
+        private string _nombre;
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private string _apellidos;
+        public string apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+
+        public bool createAdmin()
+        {
+            return false;
+        }
+
+        public bool readAdmin()
+        {
+            return false;
+        }
+
+        public bool updateAdmin()
+        {
+            return false;
+        }
+
+        public bool deleteAdmin()
+        {
+            return false;
+        }
     }
 }
