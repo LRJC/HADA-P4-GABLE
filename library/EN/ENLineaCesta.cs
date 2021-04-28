3847a2e2b155c2a690e199398a0cb5ef8187dcb4
Alejandro <aom54@alu.ua.es>
2021-04-28 12:37:03 +0200

    20082363M Clases Cesta y LineaCesta reformuladas

---
+++ library/EN/ENLineaCesta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using library.CAD;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library.EN
+{
+    public class ENLineaCesta
+    {
+        private ENProductos producto;
+        private int unidades;
+
+        public ENProductos Producto
+        {
+            get { return this.producto; }
+            set { this.producto = value; }
+        }
+        public int Unidades
+        {
+            get { return this.unidades; }
+            set { this.unidades = value; }
+        }
+
+        public ENLineaCesta()
+        {
+            this.producto = null;
+            this.unidades = 0;
+        }
+        public ENLineaCesta(ENProductos producto, int unidades)
+        {
+            this.producto = producto;
+            this.unidades = unidades;
+        }
+
+        public bool addUnit()
+        {
+            this.unidades += 1;
+
+            CADLineaCesta lc = new CADLineaCesta();
+            return lc.addUnit(this);
+        }
+
+        public bool removeUnit()
+        {
+            if (this.unidades > 0)
+            {
+                this.unidades += 1;
+
+                CADLineaCesta lc = new CADLineaCesta();
+                return lc.removeUnit(this);
+            }
+
+            return false;
+        }
+    }
+}
