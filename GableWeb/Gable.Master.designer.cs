77fd8176f81b9926c55397f0a2e3738ff30347c6
Alejandro <aom54@alu.ua.es>
2021-04-30 17:42:57 +0200

    20082363M Troubleshoot responsive header

--- GableWeb/Gable.Master.designer.cs
+++ GableWeb/Gable.Master.designer.cs
@@ -33,13 +33,49 @@ namespace GableWeb
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control Menu.
+        /// Control hamburger.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton Menu;
+        protected global::System.Web.UI.WebControls.ImageButton hamburger;
+
+        /// <summary>
+        /// Control gable_logo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton gable_logo;
+
+        /// <summary>
+        /// Control searcher.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox searcher;
+
+        /// <summary>
+        /// Control init_sesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label init_sesion;
+
+        /// <summary>
+        /// Control basket_icon.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton basket_icon;
 
         /// <summary>
         /// Control ImageButton1.
