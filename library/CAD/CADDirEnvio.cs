ad770cfc3f5c9da9a885a069e91bd4367924b730
Andrés Maciá Gómez <andremacia45@gmail.com>
2021-05-03 12:22:48 +0200

    EN y CAD's implementadas (TipoProducto , DirEnvio)

---
+++ library/CAD/CADDirEnvio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+	public class CADDirEnvio
+	{
+		private string constring{ get; set;}
+
+		public CADDirEnvio()
+		{
+			constring = ConfigurationManager.ConnectionStrings["miConexion"].ToString();
+		}
+
+		public bool createDirEnvio(ENDirEnvio dir) { return false; }
+		public bool readDirEnvio(ENDirEnvio dir) { return false; }
+		public bool updateDirEnvio(ENDirEnvio dir) { return false; }
+		public bool deleteDirEnvio(ENDirEnvio dir) { return false; }
+
+	}
+}
---
+++ library/CAD/CADTipoProducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+	public class CADTipoProducto
+	{
+		private string constring { get; set; }
+
+		public CADTipoProducto()
+		{
+			constring = ConfigurationManager.ConnectionStrings["miConexion"].ToString();
+		}
+
+		public bool createTipoProducto(ENTipoProducto tip) { return false; }
+		public bool readTipoProducto(ENTipoProducto tip) { return false; }
+		public bool updateTipoProducto(ENTipoProducto tip) { return false; }
+		public bool deleteTipoProducto(ENTipoProducto tip) { return false; }
+	}
+
+}
