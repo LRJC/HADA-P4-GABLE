fedc6c819b4d3b79729522e653697e06bff4ffe7
nicoslaptopW <nvog1@gcloud.ua.es>
2021-05-19 00:44:48 +0200

    74383864R localidad done

--- library/CAD/CADLocalidad.cs
+++ library/CAD/CADLocalidad.cs
@@ -27,10 +27,10 @@ namespace library
             try
             {
                 c.Open();
-                SqlCommand com1 = new SqlCommand("select * from localidad where nif = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
+                SqlCommand com1 = new SqlCommand("select * from localidad where pueblo = '" + loc.pueblo + "' AND provincia = '" + loc.provincia + "';", c);
                 SqlDataReader dr = com1.ExecuteReader();
 
-                if (dr.Read()) return false;//comprobamos qeu no exista
+                if (dr.Read()) return false;//comprobamos que no exista
                 dr.Close();
 
                 SqlCommand com2 = new SqlCommand("Insert into localidad(codm,pueblo,provincia) VALUES ('" + loc.codm + "','" + loc.pueblo + "','" + loc.provincia + "')", c);
@@ -55,6 +55,19 @@ namespace library
             try
             {
                 c.Open();
+                SqlCommand com = new SqlCommand("select * from localidad where pueblo = '" + loc.pueblo + "' AND provincia = '" + loc.provincia + "';", c);
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    loc.codm = dr["codm"].ToString();
+                    loc.pueblo = dr["pueblo"].ToString();
+                    loc.provincia = dr["provincia"].ToString();
+                    return true;
+
+                }
+
                 return false;
             }
             catch (Exception ex)
@@ -74,6 +87,18 @@ namespace library
             try
             {
                 c.Open();
+                SqlCommand com = new SqlCommand("select * from localidad order by provincia, codm ASC", c);//checkear sintaxis order by con dos columnas
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    loc.codm = dr["codm"].ToString();
+                    loc.pueblo = dr["pueblo"].ToString();
+                    loc.provincia = dr["provincia"].ToString();
+                    return true;
+                }
+
                 return false;
             }
             catch (Exception ex)
@@ -93,6 +118,25 @@ namespace library
             try
             {
                 c.Open();
+                SqlCommand com = new SqlCommand("select * from localidad order by provincia, codm ASC", c);//checkear sintaxis order by con dos columnas
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    if (dr["codm"].ToString() == loc.codm && dr["provincia"].ToString() == loc.provincia)
+                    {
+                        if (dr.Read())
+                        {
+                            loc.codm = dr["codm"].ToString();
+                            loc.pueblo = dr["pueblo"].ToString();
+                            loc.provincia = dr["provincia"].ToString();
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+
                 return false;
             }
             catch (Exception ex)
@@ -113,12 +157,12 @@ namespace library
             {
                 c.Open();
                 if (!loc.pueblo.Length() > 0) {//comprobamos que no esté vacío el campo
-                    SqlCommand com1 = new SqlCommand("update localidad set pueblo = '" + loc.pueblo + "' where nif = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
+                    SqlCommand com1 = new SqlCommand("update localidad set pueblo = '" + loc.pueblo + "' where codm = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
                     if (com1.ExecuteNonQuery() == 0) return false;
                 }
                 if(loc.provincia.Length() > 0)
                 {
-                    SqlCommand com2 = new SqlCommand("update localidad set provincia = '" + loc.provincia + "' where nif = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
+                    SqlCommand com2 = new SqlCommand("update localidad set provincia = '" + loc.provincia + "' where codm = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
                     if (com2.ExecuteNonQuery() == 0) return false;
                 }
                 
@@ -142,7 +186,7 @@ namespace library
             try
             {
                 c.Open();
-                SqlCommand com = new SqlCommand("delete from localidad where nif = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
+                SqlCommand com = new SqlCommand("delete from localidad where codm = '" + loc.codm + "' AND provincia = '" + loc.provincia + "';", c);
 
                 if (com.ExecuteNonQuery() == 0) return false;
                 return true;
