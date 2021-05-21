23bae766d6ef8ac86c14ee7bb1435cf119d7cbb6
alatorre <alg133@alu.ua.es>
2021-05-21 13:10:54 +0200

    Login/Registro/DatosCompra

---
+++ GableWeb/DatosCompra.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Gable.Master" AutoEventWireup="true" CodeBehind="DatosCompra.aspx.cs" Inherits="GableWeb.DatosCompra" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+ <div class="container-fluid ">
+        <div class="row">
+            <div class="col-sm-1" style="background-color:#e0e0e0!important"></div>
+            <div class="col-xs-12 col-sm-10" style="background-color:white; border-left: 1px solid #75777d!important; border-right: 1px solid #75777d!important; margin-top: 1px solid #75777d!important;">
+                <div class="container-fluid px-5 py-5">
+                    <div class="panel panel-default" >
+                        <div class="header">
+                            <h3 class="font-weight-bold text-center" >Introducir Datos de Compra</h3>
+                        </div>
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <span class="help-block text-muted small-font" > Numero Tarjeta</span>
+                                    <asp:TextBox ID="numTarjeta" runat="server" ></asp:TextBox>
+                                </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <span class="help-block text-muted small-font" >  CCV</span>
+                                    <asp:TextBox ID="ccv" runat="server" ></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <span class="help-block text-muted small-font" > Mes de Vencimiento</span>
+                                    <asp:TextBox ID="mesExp" runat="server" placeholder="MM"></asp:TextBox>
+                            </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <span class="help-block text-muted small-font" > Año de Vencimiento</span>
+                                    <asp:TextBox ID="yearExp" runat="server" placeholder="YYYY"></asp:TextBox>
+                                </div>
+                            </div>
+                             <div class="row justify-content-center ">
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    
+                                    <asp:Button ID="cancel" runat="server" OnClick="btnCancel_Click" Text="CANCEL" BackColor="RED" ></asp:Button>
+                                </div>
+                                <div class="col-md-2 col-sm-2 col-xs-2 offset-1">
+                                    <asp:Button ID="confirm" runat="server" OnClick="btnConfirm_Click" Text="CONFIRMAR" BackColor="Gold"></asp:Button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+    <div class="col-sm-1" style="background-color:#e0e0e0!important"></div>
+        </div>
+    </div>
+</asp:Content>
---
+++ GableWeb/Registro.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace GableWeb
+{
+
+
+    public partial class Registro
+    {
+
+        /// <summary>
+        /// Control nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombre;
+
+        /// <summary>
+        /// Control apellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox apellidos;
+
+        /// <summary>
+        /// Control tlf.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tlf;
+
+        /// <summary>
+        /// Control dni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox dni;
+
+        /// <summary>
+        /// Control date.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox date;
+
+        /// <summary>
+        /// Control email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+
+        /// <summary>
+        /// Control pwd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox pwd;
+
+        /// <summary>
+        /// Control c_pwd.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox c_pwd;
+
+        /// <summary>
+        /// Control b_register.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button b_register;
+    }
+}
