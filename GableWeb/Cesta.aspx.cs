6af60165e8b1274cd1a8f2132651fadbe42ad594
inmortalkaktus <alejandroolivaresmompo@hotmail.com>
2021-05-25 16:09:37 +0200

    20082363M Cesta.aspx.cs Conexiones de BBDD a CAD

--- GableWeb/Cesta.aspx.cs
+++ GableWeb/Cesta.aspx.cs
@@ -13,8 +13,10 @@ namespace GableWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private ENCesta cesta;
         protected void Page_Load(object sender, EventArgs e)
         {
+            cesta = new ENCesta();
             if(!IsPostBack && CheckIfSomeItem())
             {
                 ShowBasketItems();
@@ -23,30 +25,11 @@ namespace GableWeb
 
         private void ShowBasketItems()
         {
-            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-            using (SqlConnection con = new SqlConnection(constring))
-            {
-                using(SqlCommand cmd = new SqlCommand("" +
-                    "select p.nombre, p.imagen, lc.numCesta, lc.linea, " +
-                    "lc.importe, lc.cantidad, lc.importe*lc.cantidad as total " +
-                    "from linCest lc " +
-                    "inner join producto p " +
-                    "on lc.producto = p.id_producto " +
-                    "inner join cesta c " +
-                    "on c.numCesta = lc.numCesta and c.usuario = '20094273D'", con))
-                    // TO DO: Detect user session in c.usuario
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable linCestData = new DataTable();
-                        sda.Fill(linCestData);
-                        itemsCesta.DataSource = linCestData;
-                        itemsCesta.DataBind();
-                        double finalPrice = linCestData.Select().Sum(p => Convert.ToDouble(p["total"]));
-                        (itemsCesta.Controls[itemsCesta.Controls.Count - 1].Controls[0].FindControl("totalPrice") as Label).Text = finalPrice.ToString() + "€";
-                    }
-                }
-            }
+            DataTable linCestData = cesta.ShowBasketItems();
+            itemsCesta.DataSource = linCestData;
+            itemsCesta.DataBind();
+            double finalPrice = linCestData.Select().Sum(p => Convert.ToDouble(p["total"]));
+            (itemsCesta.Controls[itemsCesta.Controls.Count - 1].Controls[0].FindControl("totalPrice") as Label).Text = finalPrice.ToString() + "€";
         }
 
         protected void ItemQuery(object sender, CommandEventArgs e)
@@ -106,146 +89,28 @@ namespace GableWeb
 
         private void InsertItemsIntoOrders(int numCesta)
         {
-            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-            using (SqlConnection con = new SqlConnection(constring))
-            {
-                using (SqlCommand cmd = new SqlCommand("" +
-                    "select *" +
-                    "from linCest " +
-                    "where numCesta = " + numCesta +
-                    "", con))
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable linCestData = new DataTable();
-                        sda.Fill(linCestData);
-
-                        foreach (DataRow row in linCestData.Rows)
-                        {
-                            SqlCommand rowCmd = new SqlCommand("" +
-                                "insert into linPed values (" +
-                                row[0] + "," + row[1] + "," + row[2] +
-                                "," + row[3] + "," + row[4], con);
-
-                            rowCmd.ExecuteNonQuery();
-                        }
-                    }
-                }
-            }
+            cesta.InsertItemsIntoOrders(numCesta);
         }
 
         private void DeleteItemsFromBasket(int numCesta)
         {
-            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-            using (SqlConnection con = new SqlConnection(constring))
-            {
-                using (SqlCommand cmd = new SqlCommand("" +
-                    "select *" +
-                    "from linCest " +
-                    "where numCesta = " + numCesta +
-                    "", con))
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable linCestData = new DataTable();
-                        sda.Fill(linCestData);
-
-                        foreach (DataRow row in linCestData.Rows)
-                        {
-                            SqlCommand rowCmd = new SqlCommand("" +
-                                "delete from linCest " +
-                                "where numCesta = " + numCesta + " " +
-                                "and linea = " + row[1], con);
-
-                            rowCmd.ExecuteNonQuery();
-                        }
-                    }
-                }
-            }
+            cesta.DeleteItemsFromBasket(numCesta);
         }
 
         private bool CheckIfSomeItem()
         {
-            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-            using (SqlConnection con = new SqlConnection(constring))
-            {
-                using (SqlCommand cmd = new SqlCommand("" +
-                    "select * " +
-                    "from linCest " +
-                    "", con))
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable linCestData = new DataTable();
-                        sda.Fill(linCestData);
-
-                        return linCestData.Rows.Count > 0;
-                    }
-                }
-            }
+            return cesta.CheckIfSomeItem();
         }
 
         private bool CheckIfItemExists(int numCesta, int linea)
         {
-            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-            using (SqlConnection con = new SqlConnection(constring))
-            {
-                using (SqlCommand cmd = new SqlCommand("" +
-                    "select * " +
-                    "from linCest " +
-                    "where numCesta = " + numCesta + " and linea = " + linea +
-                    "", con))
-                {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable linCestData = new DataTable();
-                        sda.Fill(linCestData);
-
-                        return linCestData.Rows.Count > 0;
-                    }
-                }
-            }
+            return cesta.CheckIfItemExists(numCesta, linea);
         }
 
         private bool CheckIfHaveBuyData(int numCesta)
         {
             ENCesta c = new ENCesta(numCesta);
-            string userID = c.getUserID();
-
-            if(userID != null)
-            {
-                string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
-                using (SqlConnection con = new SqlConnection(constring))
-                {
-                    using (SqlCommand cmd = new SqlCommand("" +
-                        "select u.tarjeta, de.provincia, de.pueblo, de.calle" +
-                        "from usuario u, dirEnvio de " +
-                        "where u.dni = de.usuario" +
-                        "", con))
-                    {
-                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                        {
-                            DataTable userData = new DataTable();
-                            sda.Fill(userData);
-
-                            if(userData.Rows.Count > 0)
-                            {
-                                bool haveAllDependencies = true;
-
-                                for(int i = 0; i < userData.Columns.Count; i++)
-                                {
-                                    if (userData.Rows[0][i] == DBNull.Value)
-                                        haveAllDependencies = false;
-                                }
-
-                                return haveAllDependencies;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return false;
+            return c.CheckIfHaveBuyData(numCesta);
         }
     }
 }
--- library/CAD/CADCesta.cs
+++ library/CAD/CADCesta.cs
@@ -44,7 +44,171 @@ namespace library.CAD
             }
         }
 
-        public bool proceedToBuy(ENCesta c) { return false; }
+        public bool ProceedToBuy(ENCesta c) { return false; }
+        public DataTable ShowBasketItems() {
+            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("" +
+                    "select p.nombre, p.imagen, lc.numCesta, lc.linea, " +
+                    "lc.importe, lc.cantidad, lc.importe*lc.cantidad as total " +
+                    "from linCest lc " +
+                    "inner join producto p " +
+                    "on lc.producto = p.id_producto " +
+                    "inner join cesta c " +
+                    "on c.numCesta = lc.numCesta and c.usuario = '20094273D'", con))
+                // TO DO: Detect user session in c.usuario
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable linCestData = new DataTable();
+                        sda.Fill(linCestData);
+                        return linCestData;
+                    }
+                }
+            }
+        }
+
+        public void InsertItemsIntoOrders(int numCesta)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("" +
+                    "select *" +
+                    "from linCest " +
+                    "where numCesta = " + numCesta +
+                    "", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable linCestData = new DataTable();
+                        sda.Fill(linCestData);
+
+                        foreach (DataRow row in linCestData.Rows)
+                        {
+                            SqlCommand rowCmd = new SqlCommand("" +
+                                "insert into linPed values (" +
+                                row[0] + "," + row[1] + "," + row[2] +
+                                "," + row[3] + "," + row[4], con);
+
+                            rowCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DeleteItemsFromBasket(int numCesta)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("" +
+                    "select *" +
+                    "from linCest " +
+                    "where numCesta = " + numCesta +
+                    "", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable linCestData = new DataTable();
+                        sda.Fill(linCestData);
+
+                        foreach (DataRow row in linCestData.Rows)
+                        {
+                            SqlCommand rowCmd = new SqlCommand("" +
+                                "delete from linCest " +
+                                "where numCesta = " + numCesta + " " +
+                                "and linea = " + row[1], con);
+
+                            rowCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool CheckIfSomeItem()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("" +
+                    "select * " +
+                    "from linCest " +
+                    "", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable linCestData = new DataTable();
+                        sda.Fill(linCestData);
+
+                        return linCestData.Rows.Count > 0;
+                    }
+                }
+            }
+        }
+
+        public bool CheckIfItemExists(int numCesta, int linea)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("" +
+                    "select * " +
+                    "from linCest " +
+                    "where numCesta = " + numCesta + " and linea = " + linea +
+                    "", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable linCestData = new DataTable();
+                        sda.Fill(linCestData);
+
+                        return linCestData.Rows.Count > 0;
+                    }
+                }
+            }
+        }
+
+        public bool CheckIfHaveBuyData(string userID, int numCesta)
+        {
+            if (userID != null)
+            {
+                string constring = ConfigurationManager.ConnectionStrings["bbdd"].ToString();
+                using (SqlConnection con = new SqlConnection(constring))
+                {
+                    using (SqlCommand cmd = new SqlCommand("" +
+                        "select u.tarjeta, de.provincia, de.pueblo, de.calle" +
+                        "from usuario u, dirEnvio de " +
+                        "where u.dni = de.usuario" +
+                        "", con))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            DataTable userData = new DataTable();
+                            sda.Fill(userData);
+
+                            if (userData.Rows.Count > 0)
+                            {
+                                bool haveAllDependencies = true;
+
+                                for (int i = 0; i < userData.Columns.Count; i++)
+                                {
+                                    if (userData.Rows[0][i] == DBNull.Value)
+                                        haveAllDependencies = false;
+                                }
+
+                                return haveAllDependencies;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
 
     }
 }
