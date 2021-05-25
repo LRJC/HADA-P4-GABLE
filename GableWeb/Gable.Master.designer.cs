8d16080ffd6efc980357ce243e75de5318e2d5e0
Lucas Jeacock <ljc11@gcloud.ua.es>
2021-05-26 01:12:28 +0200

    algunas navigateURL añadidas

--- GableWeb/Gable.Master.designer.cs
+++ GableWeb/Gable.Master.designer.cs
@@ -33,6 +33,15 @@ namespace GableWeb
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control Menu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu;
+
+        /// <summary>
         /// Control gable_logo.
         /// </summary>
         /// <remarks>
