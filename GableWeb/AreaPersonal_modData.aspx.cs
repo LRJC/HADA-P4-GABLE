e1309abae80bf19421edaa2cbb509d413afcdb30
nicoslaptopW <nvog1@gcloud.ua.es>
2021-05-31 16:02:01 +0200

    74383864R set client data in areapersonal

--- GableWeb/AreaPersonal_modData.aspx.cs
+++ GableWeb/AreaPersonal_modData.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using library;
 
 namespace GableWeb
 {
@@ -11,7 +12,32 @@ namespace GableWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["dni"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                setClientArea(Session["dni"].ToString());
+            }
+        }
 
+        protected void setClientArea(string dni)
+        {
+            ENUsuario usu = new ENUsuario();
+            usu.nif = dni;
+            if (!usu.readUsuario())//no existe el usuario de la sesión actual
+            {
+                Session.RemoveAll();
+                Session.Abandon();
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                lab_NombreApellidos_AreaPersonal_mod.Text = usu.nombre.ToString() + " " + usu.apellidos.ToString();
+                lab_Dni_AreaPersonal_mod.Text = usu.nif.ToString();
+                lab_Correo_AreaPersonal_mod.Text = usu.email.ToString();
+            }
         }
 
         protected void ButtonCambiarContra_Click(object sender, EventArgs e)
